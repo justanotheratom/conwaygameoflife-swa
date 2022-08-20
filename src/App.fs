d0a01bb6ccf853fdbabc4f8c6c9a4a8dc39f7808
justanotheratom <sanketpatel.1805@gmail.com>
2022-08-20 07:08:48 +0000

    Remove code related to Azure Functions

--- src/App.fs
+++ src/App.fs
@@ -14,30 +14,7 @@ let Counter () =
                              prop.onClick (fun _ -> setCount (count + 1)) ] ]
 
 [<ReactComponent>]
-let Message () =
-    let (message, setMessage) = React.useState ("")
-
-    Html.div [ Html.button [ prop.text "Get a message from the API"
-                             prop.onClick
-                                 (fun _ ->
-                                     promise {
-                                         let! message =
-                                             Fetch.get (
-                                                 "/api/GetMessage?name=FSharp",
-                                                 headers = [ HttpRequestHeaders.Accept "application/json" ]
-                                             )
-
-                                         setMessage message
-                                         return ()
-                                     }
-                                     |> ignore) ]
-               if message = "" then
-                   Html.none
-               else
-                   Html.p message ]
-
-[<ReactComponent>]
-let App () = React.fragment [ Counter(); Message() ]
+let App () = React.fragment [ Counter(); ]
 
 open Browser.Dom
 
